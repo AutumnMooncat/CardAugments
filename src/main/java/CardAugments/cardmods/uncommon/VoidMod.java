721dbc904e7edd278c3e41869376e26394d73ffa
Alison Turner <alisonmoons@gmail.com>
2022-02-17 19:33:41 -0400

    Void mod

---
+++ src/main/java/CardAugments/cardmods/uncommon/VoidMod.java
@@ -0,0 +1,70 @@
+package CardAugments.cardmods.uncommon;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.status.VoidCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+public class VoidMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("VoidMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    private static final int VOIDS = 1;
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.cost > 0 && isNormalCard(card);
+    }
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        super.onInitialApplication(card);
+        if (card.cardsToPreview == null) {
+            card.cardsToPreview = new VoidCard();
+        }
+        card.cost -= 1;
+        card.costForTurn = card.cost;
+    }
+
+    @Override
+    public boolean shouldApply(AbstractCard card) {
+        AbstractCard upgradeCheck = card.makeCopy();
+        upgradeCheck.upgrade();
+        return card.cost == upgradeCheck.cost && validCard(card);
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return rawDescription + String.format(TEXT[2], VOIDS);
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        this.addToBot(new MakeTempCardInDiscardAction(new VoidCard(), VOIDS));
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new VoidMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
