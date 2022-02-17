180cb07f64a66d3841d3abb85bbc4ea80b912078
Alison Turner <alisonmoons@gmail.com>
2022-02-17 18:21:38 -0400

    Added new stat scaling logic to streamline cards that modify base stats

--- src/main/java/CardAugments/cardmods/AbstractAugment.java
+++ src/main/java/CardAugments/cardmods/AbstractAugment.java
@@ -12,6 +12,67 @@ public abstract class AbstractAugment extends AbstractCardModifier {
         RARE
     }
 
+    public enum BuffType {
+        DAMAGE,
+        BLOCK,
+        MAGIC
+    }
+
+    public enum BuffScale {
+        MAJOR_BUFF,
+        MODERATE_BUFF,
+        MINOR_BUFF,
+        MINOR_DEBUFF,
+        MODERATE_DEBUFF,
+        MAJOR_DEBUFF,
+    }
+
+    public void modifyBaseStat(AbstractCard card, BuffType type, BuffScale scaling) {
+        AbstractCard upgradeCheck = card.makeCopy();
+        upgradeCheck.upgrade();
+        switch (type) {
+            case DAMAGE:
+                card.baseDamage += getStatModification(Math.max(card.baseDamage, upgradeCheck.baseDamage), scaling);
+                if (card.baseDamage < 1) {
+                    card.baseDamage = 1;
+                }
+                card.damage = card.baseDamage;
+                break;
+            case BLOCK:
+                card.baseBlock += getStatModification(Math.max(card.baseBlock, upgradeCheck.baseBlock), scaling);
+                if (card.baseBlock < 1) {
+                    card.baseBlock = 1;
+                }
+                card.block = card.baseBlock;
+                break;
+            case MAGIC:
+                card.baseMagicNumber += getStatModification(Math.max(card.baseMagicNumber, upgradeCheck.baseMagicNumber), scaling);
+                if (card.baseMagicNumber < 1) {
+                    card.baseMagicNumber = 1;
+                }
+                card.magicNumber = card.baseMagicNumber;
+                break;
+        }
+    }
+
+    public int getStatModification(int baseStat, BuffScale scaling) {
+        switch (scaling) {
+            case MAJOR_BUFF:
+                return (int) (Math.ceil(baseStat/3f) + 1);
+            case MODERATE_BUFF:
+                return (int) Math.ceil(baseStat/3f);
+            case MINOR_BUFF:
+                return (int) Math.ceil(baseStat/5f);
+            case MINOR_DEBUFF:
+                return (int) -Math.ceil(baseStat/5f);
+            case MODERATE_DEBUFF:
+                return (int) -Math.ceil(baseStat/3f);
+            case MAJOR_DEBUFF:
+                return (int) -(Math.ceil(baseStat/3f) + 1);
+        }
+        return 0;
+    }
+
     public abstract AugmentRarity getModRarity();
 
     public abstract boolean validCard(AbstractCard card);
