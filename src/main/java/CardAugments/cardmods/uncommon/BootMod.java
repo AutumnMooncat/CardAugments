0fa563f14b076a9fa351644fe9d50148846280ad
Alison Turner <alisonmoons@gmail.com>
2022-02-17 22:46:34 -0400

    Added Sticky (Retain) and Boot (Innate) mods

---
+++ src/main/java/CardAugments/cardmods/uncommon/BootMod.java
@@ -0,0 +1,47 @@
+package CardAugments.cardmods.uncommon;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+public class BootMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("BootMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        card.isInnate = true;
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return isNormalCard(card) && !card.isInnate;
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return TEXT[2] + rawDescription;
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new BootMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
---
+++ src/main/java/CardAugments/cardmods/uncommon/StickyMod.java
@@ -0,0 +1,47 @@
+package CardAugments.cardmods.uncommon;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+public class StickyMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("StickyMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        card.selfRetain = true;
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return isNormalCard(card) && !card.selfRetain;
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return TEXT[2] + rawDescription;
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new StickyMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
