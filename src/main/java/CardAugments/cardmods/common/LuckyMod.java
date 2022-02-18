b9c24986b09dd7bfe84cca598794ae8249231e15
Alison Turner <alisonmoons@gmail.com>
2022-02-18 10:22:27 -0400

    Added Lucky mod

---
+++ src/main/java/CardAugments/cardmods/common/LuckyMod.java
@@ -0,0 +1,52 @@
+package CardAugments.cardmods.common;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+public class LuckyMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("LuckyMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    private static final int SCRY = 1;
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.cost != -2;
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return rawDescription + String.format(TEXT[2], SCRY);
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        this.addToBot(new ScryAction(SCRY));
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.COMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new LuckyMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
