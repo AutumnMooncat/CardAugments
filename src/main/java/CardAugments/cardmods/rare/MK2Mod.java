c4dbe52e5854502485700dd4deff5347b2369c03
Alison Turner <alisonmoons@gmail.com>
2022-02-17 20:49:51 -0400

    Added MK2 mod

---
+++ src/main/java/CardAugments/cardmods/rare/MK2Mod.java
@@ -0,0 +1,60 @@
+package CardAugments.cardmods.rare;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+public class MK2Mod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("MK2Mod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        AbstractCard upgradeCheck = card.makeCopy();
+        upgradeCheck.upgrade();
+        if (card.baseDamage > 0) {
+            modifyBaseStat(card, BuffType.DAMAGE, BuffScale.MINOR_BUFF);
+        }
+        if (card.baseBlock > 0) {
+            modifyBaseStat(card, BuffType.BLOCK, BuffScale.MINOR_BUFF);
+        }
+        if (usesMagic(card) && card.baseMagicNumber <= upgradeCheck.baseMagicNumber) {
+            modifyBaseStat(card, BuffType.MAGIC, BuffScale.MINOR_BUFF);
+        }
+
+    }
+
+    @Override
+    public boolean shouldApply(AbstractCard card) {
+        AbstractCard upgradeCheck = card.makeCopy();
+        upgradeCheck.upgrade();
+        return card.baseMagicNumber <= upgradeCheck.baseMagicNumber && validCard(card);
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return (card.baseDamage > 0 || card.baseBlock > 0 || usesMagic(card)) && isNormalCard(card);
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.RARE;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new MK2Mod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
