ec484e9836e7949b26b4247d8904eebfc009e3a2
Alison Turner <alisonmoons@gmail.com>
2022-02-17 23:28:04 -0400

    Added Auto mod

---
+++ src/main/java/CardAugments/actions/AutoplayOnRandomEnemyAction.java
@@ -0,0 +1,25 @@
+package CardAugments.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class AutoplayOnRandomEnemyAction extends AbstractGameAction {
+    AbstractCard card;
+
+    public AutoplayOnRandomEnemyAction(AbstractCard card) {
+        this.card = card;
+    }
+
+    @Override
+    public void update() {
+        if (card != null && AbstractDungeon.actionManager.cardQueue.stream().noneMatch(i -> i.card == card)) {
+            card.targetAngle = 0.0F;
+            AbstractDungeon.actionManager.cardQueue.add(new CardQueueItem(this.card, AbstractDungeon.getRandomMonster()));
+        }
+
+        this.isDone = true;
+    }
+
+}
---
+++ src/main/java/CardAugments/cardmods/uncommon/AutoMod.java
@@ -0,0 +1,59 @@
+package CardAugments.cardmods.uncommon;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.actions.AutoplayOnRandomEnemyAction;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.evacipated.cardcrawl.mod.stslib.fields.cards.AbstractCard.AutoplayField;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+public class AutoMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("AutoMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    public static final int DRAW = 1;
+
+    @Override
+    public void onDrawn(AbstractCard card) {
+        addToTop(new AutoplayOnRandomEnemyAction(card));
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        addToBot(new DrawCardAction(DRAW));
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return isNormalCard(card) && !AutoplayField.autoplay.get(card);
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return TEXT[2] + rawDescription + String.format(TEXT[3], DRAW);
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new AutoMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
