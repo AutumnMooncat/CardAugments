c70a584868c16d172f6aa431d71c4ebde7aadbbf
Alison Turner <alisonmoons@gmail.com>
2022-02-19 16:44:19 -0400

    Added Hemo mod

---
+++ src/main/java/CardAugments/cardmods/uncommon/HemoMod.java
@@ -0,0 +1,61 @@
+package CardAugments.cardmods.uncommon;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class HemoMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("HemoMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    private static final int LOSS = 3;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        modifyBaseStat(card, BuffType.DAMAGE, BuffScale.HUGE_BUFF);
+        modifyBaseStat(card, BuffType.BLOCK, BuffScale.HUGE_BUFF);
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.cost != -2 && (card.baseDamage > 0 || card.baseBlock > 0) && isNormalCard(card) && !card.isInnate;
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return String.format(TEXT[2], LOSS) + rawDescription;
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        this.addToTop(new LoseHPAction(AbstractDungeon.player, AbstractDungeon.player, LOSS));
+        this.addToTop(new SFXAction("BLOOD_SPLAT", 0.8f));
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.COMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new HemoMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
