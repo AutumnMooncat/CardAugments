32e7180b8a76ef8a992219e68c18d7a8bb09583f
Alison Turner <alisonmoons@gmail.com>
2022-02-19 16:46:09 -0400

    Tear mod now uses new dynvar solution

--- src/main/java/CardAugments/cardmods/uncommon/TearMod.java
+++ src/main/java/CardAugments/cardmods/uncommon/TearMod.java
@@ -2,7 +2,8 @@ package CardAugments.cardmods.uncommon;
 
 import CardAugments.CardAugmentsMod;
 import CardAugments.actions.AndTearAction;
-import CardAugments.cardmods.AbstractDynvarAugment;
+import CardAugments.cardmods.AbstractAugment;
+import CardAugments.patches.ModVar;
 import CardAugments.util.CalcHelper;
 import basemod.abstracts.AbstractCardModifier;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
@@ -10,7 +11,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 
-public class TearMod extends AbstractDynvarAugment {
+public class TearMod extends AbstractAugment {
     public static final String ID = CardAugmentsMod.makeID("TearMod");
     public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
 
@@ -20,27 +21,25 @@ public class TearMod extends AbstractDynvarAugment {
     @Override
     public void onInitialApplication(AbstractCard card) {
         super.onInitialApplication(card);
-        baseValue = value = card.upgraded ? UPGRADE_DAMAGE : DAMAGE;
-        card.cost = card.cost + 1;
-        card.costForTurn = card.cost;
+        ModVar.setVal(card, getAmount(card));
+        ModVar.setBaseVal(card, getAmount(card));
+        modifyBaseStat(card, BuffType.DAMAGE, BuffScale.MAJOR_DEBUFF);
+        modifyBaseStat(card, BuffType.BLOCK, BuffScale.MODERATE_DEBUFF);
     }
 
     @Override
-    public boolean shouldRenderValue() {
-        return true;
-    }
-
-    @Override
-    public boolean upgraded(AbstractCard card) {
-        baseValue = value = card.upgraded ? UPGRADE_DAMAGE : DAMAGE;
-        isValueModified = card.upgraded;
-        return card.upgraded;
+    public void onUpgradeCheck(AbstractCard card) {
+        ModVar.setVal(card, getAmount(card));
+        ModVar.setBaseVal(card, getAmount(card));
+        ModVar.setModified(card, card.upgraded);
+        ModVar.setUpgraded(card, card.upgraded);
     }
 
     @Override
     public void updateDynvar(AbstractCard card) {
-        baseValue = value = card.upgraded ? UPGRADE_DAMAGE : DAMAGE;
-        isValueModified = false;
+        ModVar.setVal(card, getAmount(card));
+        ModVar.setBaseVal(card, getAmount(card));
+        ModVar.setModified(card, false);
     }
 
     @Override
@@ -52,14 +51,14 @@ public class TearMod extends AbstractDynvarAugment {
 
     @Override
     public void onApplyPowers(AbstractCard card) {
-        baseValue = card.upgraded ? UPGRADE_DAMAGE : DAMAGE;
-        value = CalcHelper.applyPowers(baseValue);
-        isValueModified = value != baseValue;
+        ModVar.setBaseVal(card, getAmount(card));
+        ModVar.setVal(card, CalcHelper.applyPowers(getAmount(card)));
+        ModVar.updateModified(card);
     }
 
     @Override
     public boolean validCard(AbstractCard card) {
-        return card.cost != -2 && isNormalCard(card);
+        return card.cost != -2 && isNormalCard(card) && (card.baseDamage > 1 || card.baseBlock > 1);
     }
 
     @Override
@@ -69,17 +68,17 @@ public class TearMod extends AbstractDynvarAugment {
 
     @Override
     public String modifyDescription(String rawDescription, AbstractCard card) {
-        return rawDescription + String.format(TEXT[2], key);
+        return rawDescription + TEXT[2];
     }
 
     @Override
     public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
-        addToBot(new AndTearAction(baseValue));
+        addToBot(new AndTearAction(ModVar.getBaseVal(card)));
     }
 
     @Override
     public AugmentRarity getModRarity() {
-        return AugmentRarity.UNCOMMON;
+        return AugmentRarity.RARE;
     }
 
     @Override
@@ -90,5 +89,9 @@ public class TearMod extends AbstractDynvarAugment {
     @Override
     public String identifier(AbstractCard card) {
         return ID;
+    }
+
+    public int getAmount(AbstractCard card) {
+        return card.upgraded ? UPGRADE_DAMAGE : DAMAGE;
     }
 }
