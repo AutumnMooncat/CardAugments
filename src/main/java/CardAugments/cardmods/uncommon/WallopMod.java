6603e80af8d7bfa0c2a0a3cf634743034fc28ced
Alison Turner <alisonmoons@gmail.com>
2022-02-19 11:20:52 -0400

    Added Wallop mod

---
+++ src/main/java/CardAugments/cardmods/uncommon/WallopMod.java
@@ -0,0 +1,58 @@
+package CardAugments.cardmods.uncommon;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import CardAugments.damagemods.WallopDamage;
+import basemod.abstracts.AbstractCardModifier;
+import com.evacipated.cardcrawl.mod.stslib.damagemods.DamageModifierManager;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+public class WallopMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("WallopMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        card.cost = card.cost + 1;
+        card.costForTurn = card.cost;
+        DamageModifierManager.addModifier(card, new WallopDamage());
+    }
+
+    @Override
+    public boolean shouldApply(AbstractCard card) {
+        AbstractCard upgradeCheck = card.makeCopy();
+        upgradeCheck.upgrade();
+        return card.cost == upgradeCheck.cost && validCard(card);
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.type == AbstractCard.CardType.ATTACK;
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return rawDescription + TEXT[2];
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new WallopMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
---
+++ src/main/java/CardAugments/damagemods/WallopDamage.java
@@ -0,0 +1,27 @@
+package CardAugments.damagemods;
+
+import com.evacipated.cardcrawl.mod.stslib.damagemods.AbstractDamageModifier;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class WallopDamage extends AbstractDamageModifier {
+
+    @Override
+    public void onLastDamageTakenUpdate(DamageInfo info, int lastDamageTaken, int overkillAmount, AbstractCreature target) {
+        if (lastDamageTaken > 0) {
+            addToBot(new GainBlockAction(AbstractDungeon.player, lastDamageTaken));
+        }
+    }
+
+    @Override
+    public boolean isInherent() {
+        return true;
+    }
+
+    @Override
+    public AbstractDamageModifier makeCopy() {
+        return new WallopDamage();
+    }
+}
