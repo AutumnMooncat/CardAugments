7f8e4a5b47ee3389869463ce10e5bac3cc2bce47
Alison Turner <alisonmoons@gmail.com>
2022-02-19 11:20:07 -0400

    Added Reshuffle mod

---
+++ src/main/java/CardAugments/cardmods/uncommon/ReshuffleMod.java
@@ -0,0 +1,47 @@
+package CardAugments.cardmods.uncommon;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+public class ReshuffleMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("ReshuffleMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        card.shuffleBackIntoDrawPile = true;
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.cost != -2 && isNormalCard(card) && !card.shuffleBackIntoDrawPile && cardDoesntExhaust(card);
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return rawDescription + TEXT[2];
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new ReshuffleMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
