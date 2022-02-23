a4c736eeba5c3d7c1e26a117c54ebc414ef2e399
Alison Turner <alisonmoons@gmail.com>
2022-02-23 17:44:58 -0400

    Added Bloody mod

---
+++ src/main/java/CardAugments/cardmods/uncommon/BloodyMod.java
@@ -0,0 +1,63 @@
+package CardAugments.cardmods.uncommon;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+public class BloodyMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("BloodyMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        card.cost = card.cost + 1;
+        card.costForTurn = card.cost;
+    }
+
+    @Override
+    public boolean shouldApply(AbstractCard card) {
+        AbstractCard upgradeCheck = card.makeCopy();
+        upgradeCheck.upgrade();
+        return card.cost == upgradeCheck.cost && validCard(card);
+    }
+
+    @Override
+    public void onDamaged(AbstractCard c) {
+        c.updateCost(-1);
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.cost >= 1;
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        if (rawDescription.contains(TEXT[4])) {
+            return rawDescription.replace(TEXT[4], TEXT[5]);
+        }
+        return TEXT[2] + rawDescription;
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new BloodyMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
