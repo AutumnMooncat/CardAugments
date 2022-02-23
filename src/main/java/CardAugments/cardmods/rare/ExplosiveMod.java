5a905de68b1b45a2905d900af5e5185e57a73494
Alison Turner <alisonmoons@gmail.com>
2022-02-23 17:31:25 -0400

    Added Explosive mod

---
+++ src/main/java/CardAugments/cardmods/rare/ExplosiveMod.java
@@ -0,0 +1,74 @@
+package CardAugments.cardmods.rare;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.colorless.TheBomb;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.TheBombPower;
+
+public class ExplosiveMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("ExplosiveMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    private static final int EFFECT = 20;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        if (card instanceof TheBomb) {
+            card.baseMagicNumber += EFFECT;
+        }
+        card.cost = card.cost + 1;
+        card.costForTurn = card.cost;
+    }
+
+    @Override
+    public boolean shouldApply(AbstractCard card) {
+        AbstractCard upgradeCheck = card.makeCopy();
+        upgradeCheck.upgrade();
+        return card.cost == upgradeCheck.cost && validCard(card);
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.cost >= 0 && card.rarity != AbstractCard.CardRarity.BASIC && card.rarity != AbstractCard.CardRarity.COMMON;
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        if (card instanceof TheBomb) {
+            return rawDescription;
+        }
+        return rawDescription + String.format(TEXT[2], EFFECT);
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        this.addToBot(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new TheBombPower(AbstractDungeon.player, 3, EFFECT), 3));
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.RARE;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new ExplosiveMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
