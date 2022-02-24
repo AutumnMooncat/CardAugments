f5a45731d5b40bbdf18f241056a8a9c6a60584e4
Alison Turner <alisonmoons@gmail.com>
2022-02-24 00:08:43 -0400

    Added Follow-Up mod

---
+++ src/main/java/CardAugments/cardmods/common/FollowUpMod.java
@@ -0,0 +1,64 @@
+package CardAugments.cardmods.common;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.actions.watcher.FollowUpAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class FollowUpMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("FollowUpMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        modifyBaseStat(card, BuffType.DAMAGE, BuffScale.MINOR_DEBUFF);
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.baseDamage > 1 && card.type == AbstractCard.CardType.ATTACK;
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        if (rawDescription.contains(TEXT[4])) {
+            return rawDescription.replace(TEXT[4], TEXT[5]);
+        }
+        return rawDescription + TEXT[2];
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        this.addToBot(new FollowUpAction());
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.COMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new FollowUpMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+
+    @Override
+    public boolean glowCheck(AbstractCard card) {
+        return !AbstractDungeon.actionManager.cardsPlayedThisCombat.isEmpty() && (AbstractDungeon.actionManager.cardsPlayedThisCombat.get(AbstractDungeon.actionManager.cardsPlayedThisCombat.size() - 1)).type == AbstractCard.CardType.ATTACK;
+    }
+}
