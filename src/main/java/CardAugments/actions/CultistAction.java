ae5feb4492fe0244d3ff2255b2a169e46cf89824
Alison Turner <alisonmoons@gmail.com>
2022-02-24 22:25:13 -0400

    Fixed Cultist action order and added proper amount to Str Power

--- src/main/java/CardAugments/actions/CultistAction.java
+++ src/main/java/CardAugments/actions/CultistAction.java
@@ -20,14 +20,14 @@ public class CultistAction extends AbstractGameAction {
         if (AbstractDungeon.actionManager.cardsPlayedThisCombat.size() >= 2) {
             int amount = AbstractDungeon.actionManager.cardsPlayedThisCombat.get(AbstractDungeon.actionManager.cardsPlayedThisCombat.size() - 2).cost;
             if (amount == -1) {
-                amount = EnergyPanel.totalCount;
+                amount = AbstractDungeon.player.energy.energyMaster;
             }
+            this.addToTop(new TalkAction(true, CardCrawlGame.languagePack.getRelicStrings(CultistMask.ID).DESCRIPTIONS[1], 0.0F, 2.0F));
             if (amount > 0) {
-                this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new StrengthPower(AbstractDungeon.player, 1)));
-                this.addToTop(new VFXAction(AbstractDungeon.player, new InflameEffect(AbstractDungeon.player), 0.3F));
-                this.addToTop(new SFXAction("VO_CULTIST_1A"));
-                this.addToTop(new TalkAction(true, CardCrawlGame.languagePack.getRelicStrings(CultistMask.ID).DESCRIPTIONS[1], 0.0F, 2.0F));
+                this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new StrengthPower(AbstractDungeon.player, amount), amount, true));
             }
+            this.addToTop(new VFXAction(AbstractDungeon.player, new InflameEffect(AbstractDungeon.player), 0.0F));
+            this.addToTop(new SFXAction("VO_CULTIST_1A"));
         }
         this.isDone = true;
     }
