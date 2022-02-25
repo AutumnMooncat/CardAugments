c12ac8a2ef5fe0de183e6b42ff3cf0704c1333ae
Alison Turner <alisonmoons@gmail.com>
2022-02-24 21:27:08 -0400

    Added Sadistic mod

---
+++ src/main/java/CardAugments/cardmods/uncommon/SadisticMod.java
@@ -0,0 +1,53 @@
+package CardAugments.cardmods.uncommon;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class SadisticMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("SadisticMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    private static final int BOOST = 3;
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.type == AbstractCard.CardType.ATTACK && card.baseDamage > 0;
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return rawDescription + String.format(TEXT[2], BOOST);
+    }
+
+    @Override
+    public float modifyDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
+        return damage + (BOOST * target.powers.stream().filter(p -> p.type == AbstractPower.PowerType.DEBUFF).count());
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new SadisticMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+
+}
