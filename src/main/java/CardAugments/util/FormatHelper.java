70ef31a6ade8337d6270bb5b03e5f56de4071e37
Alison Turner <alisonmoons@gmail.com>
2022-02-25 17:42:46 -0400

    Added prefixWords helper function

--- src/main/java/CardAugments/util/FormatHelper.java
+++ src/main/java/CardAugments/util/FormatHelper.java
@@ -1,11 +1,21 @@
 package CardAugments.util;
 
 public class FormatHelper {
+    private static final StringBuilder newMsg = new StringBuilder();
     public static String capitalize(String str) {
         return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
 
     public static String capitalize(String str, String match) {
         return str.replace(match, capitalize(match));
+    }
+
+    public static String prefixWords(String input, String prefix) {
+        newMsg.setLength(0);
+        for (String word : input.split(" ")) {
+            newMsg.append(prefix).append(word).append(' ');
+        }
+
+        return newMsg.toString().trim();
     }
 }
