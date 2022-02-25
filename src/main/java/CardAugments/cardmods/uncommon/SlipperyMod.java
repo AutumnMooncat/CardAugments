7042351ecea20012d0a4cf4f45047354881d2c42
Alison Turner <alisonmoons@gmail.com>
2022-02-24 21:13:51 -0400

    Slippery now buffs all stats but discards the first time it is drawn

--- src/main/java/CardAugments/cardmods/uncommon/SlipperyMod.java
+++ src/main/java/CardAugments/cardmods/uncommon/SlipperyMod.java
@@ -5,20 +5,41 @@ import CardAugments.cardmods.AbstractAugment;
 import basemod.abstracts.AbstractCardModifier;
 import com.megacrit.cardcrawl.actions.common.DiscardSpecificCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.colorless.PanicButton;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 
 public class SlipperyMod extends AbstractAugment {
     public static final String ID = CardAugmentsMod.makeID("SlipperyMod");
     public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
 
+    private boolean discarded;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        AbstractCard upgradeCheck = card.makeCopy();
+        upgradeCheck.upgrade();
+        if (card.baseDamage > 0) {
+            modifyBaseStat(card, BuffType.DAMAGE, BuffScale.HUGE_BUFF);
+        }
+        if (card.baseBlock > 0) {
+            modifyBaseStat(card, BuffType.BLOCK, BuffScale.HUGE_BUFF);
+        }
+        if (usesMagic(card) && card.baseMagicNumber <= upgradeCheck.baseMagicNumber && !(card instanceof PanicButton)) {
+            modifyBaseStat(card, BuffType.MAGIC, BuffScale.HUGE_BUFF);
+        }
+    }
+
     @Override
     public boolean validCard(AbstractCard card) {
-        return card.cost == -2;
+        return card.cost == -2 || card.baseDamage > 0 || card.baseBlock > 0 || usesMagic(card);
     }
 
     @Override
     public void onDrawn(AbstractCard card) {
-        this.addToBot(new DiscardSpecificCardAction(card));
+        if (!discarded) {
+            discarded = true;
+            this.addToBot(new DiscardSpecificCardAction(card));
+        }
     }
 
     @Override
