e1e92d13f056209c90ee3c0194ccba45833c8777
wavewhyse <bijou42@gmail.com>
2022-02-26 23:35:07 -0800

    add Spot

---
+++ src/main/java/CardAugments/cardmods/uncommon/SpotMod.java
@@ -0,0 +1,77 @@
+package CardAugments.cardmods.uncommon;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import CardAugments.patches.InterruptUseCardFieldPatches;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.actions.watcher.ChangeStanceAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.purple.*;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.stances.WrathStance;
+import com.megacrit.cardcrawl.vfx.ThoughtBubble;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SpotMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID(SpotMod.class.getSimpleName());
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        card.exhaust = false;
+        if (card.target == AbstractCard.CardTarget.SELF)
+            card.target = AbstractCard.CardTarget.SELF_AND_ENEMY;
+        if (card.target == AbstractCard.CardTarget.NONE)
+            card.target = AbstractCard.CardTarget.ENEMY;
+        InterruptUseCardFieldPatches.InterceptUseField.interceptUse.set(card, true);
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.exhaust &&
+                card.target != AbstractCard.CardTarget.ALL &&
+                card.target != AbstractCard.CardTarget.ALL_ENEMY &&
+                ! card.tags.contains(AbstractCard.CardTags.HEALING);
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        if ( !(target instanceof AbstractMonster && ((AbstractMonster)target).getIntentBaseDmg() >= 0)) {
+            AbstractDungeon.effectList.add(new ThoughtBubble(AbstractDungeon.player.dialogX, AbstractDungeon.player.dialogY, 3.0F, TEXT[5], true));
+        } else {
+            card.use(AbstractDungeon.player, (AbstractMonster) target);
+        }
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        rawDescription = rawDescription.replace(TEXT[3], TEXT[4]);
+        return TEXT[2] + rawDescription;
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new SpotMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
