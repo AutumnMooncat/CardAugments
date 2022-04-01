c6ad60aed56c2dc17597fe94bfd4fc86486e9270
Alison Turner <alisonmoons@gmail.com>
2022-04-01 13:12:20 -0300

    Added Doomed mod

---
+++ src/main/java/CardAugments/cardmods/rare/DoomedMod.java
@@ -0,0 +1,53 @@
+package CardAugments.cardmods.rare;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class DoomedMod  extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID("DoomedMod");
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    private static final int BOOST = 3;
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.type == AbstractCard.CardType.ATTACK && card.baseDamage > 0 && AbstractDungeon.player.isCursed();
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return rawDescription + String.format(TEXT[2], BOOST);
+    }
+
+    @Override
+    public float modifyDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
+        return damage + (BOOST * AbstractDungeon.player.masterDeck.group.stream().filter(c -> c.type == AbstractCard.CardType.CURSE).count());
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new DoomedMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+
+}
