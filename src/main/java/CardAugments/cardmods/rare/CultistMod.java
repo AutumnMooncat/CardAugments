7ac0d5410b00e006e096d3251e19b8f86878bd5e
Alison Turner <alisonmoons@gmail.com>
2022-04-12 13:12:57 -0300

    Cultist mod now gives 1 str instead

--- src/main/java/CardAugments/cardmods/rare/CultistMod.java
+++ src/main/java/CardAugments/cardmods/rare/CultistMod.java
@@ -1,19 +1,26 @@
 package CardAugments.cardmods.rare;
 
 import CardAugments.CardAugmentsMod;
-import CardAugments.actions.CultistAction;
 import CardAugments.cardmods.AbstractAugment;
 import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.animations.TalkAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.relics.CultistMask;
+import com.megacrit.cardcrawl.vfx.combat.InflameEffect;
 
 public class CultistMod extends AbstractAugment {
     public static final String ID = CardAugmentsMod.makeID("CultistMod");
     public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
 
-    private static final int CARDS = 1;
+    private static final int EFFECT = 1;
 
     @Override
     public void onInitialApplication(AbstractCard card) {
@@ -32,12 +39,16 @@ public class CultistMod extends AbstractAugment {
 
     @Override
     public String modifyDescription(String rawDescription, AbstractCard card) {
-        return rawDescription + String.format(TEXT[2], CARDS);
+        return rawDescription + String.format(TEXT[2], EFFECT);
     }
 
     @Override
     public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
-        addToBot(new CultistAction());
+        //addToBot(new CultistAction());
+        this.addToBot(new SFXAction("VO_CULTIST_1A"));
+        this.addToBot(new VFXAction(AbstractDungeon.player, new InflameEffect(AbstractDungeon.player), 0.0F));
+        this.addToBot(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new StrengthPower(AbstractDungeon.player, EFFECT), EFFECT, true));
+        this.addToBot(new TalkAction(true, CardCrawlGame.languagePack.getRelicStrings(CultistMask.ID).DESCRIPTIONS[1], 0.0F, 2.0F));
     }
 
     @Override
