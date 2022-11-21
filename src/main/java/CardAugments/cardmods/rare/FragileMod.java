f53d41670edf42c6a5d9fd2f209bb05585cac6a4
Alison Turner <alisonmoons@gmail.com>
2022-11-20 23:02:47 -0400

    Add Fragile mod

---
+++ src/main/java/CardAugments/cardmods/rare/FragileMod.java
@@ -0,0 +1,55 @@
+package CardAugments.cardmods.rare;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+public class FragileMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID(FragileMod.class.getSimpleName());
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        card.cost = card.cost - 1;
+        card.costForTurn = card.cost;
+        card.exhaust = true;
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.cost > 0 && cardCheck(card, c -> doesntUpgradeCost() && doesntExhaust(c));
+    }
+
+    @Override
+    public String modifyName(String cardName, AbstractCard card) {
+        return TEXT[0] + cardName + TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return rawDescription + TEXT[2];
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.RARE;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new FragileMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
