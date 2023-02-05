503f7612d7f34da23663fd1472500fb78b065da1
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-02-05 13:17:27 -0400

    Add Locking and Poison Mods

---
+++ src/main/java/CardAugments/cardmods/common/PoisonedMod.java
@@ -0,0 +1,122 @@
+package CardAugments.cardmods.common;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import CardAugments.cardmods.DynvarCarrier;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PoisonPower;
+
+public class PoisonedMod extends AbstractAugment implements DynvarCarrier {
+    public static final String ID = CardAugmentsMod.makeID(PoisonedMod.class.getSimpleName());
+    public static final String DESCRIPTION_KEY = "!"+ID+"!";
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    private static final int EFFECT = 2;
+    private static final int UPGRADE_EFFECT = 1;
+
+    public int val;
+    public boolean modified;
+    public boolean upgraded;
+
+    public int getBaseVal(AbstractCard card) {
+        int upgrades = card.timesUpgraded;
+        if (upgrades == 0) {
+            return EFFECT;
+        } else if (upgrades < 0) {
+            upgrades *= -1;
+        }
+        return EFFECT + upgrades * UPGRADE_EFFECT;
+    }
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        val = getBaseVal(card);
+    }
+
+    @Override
+    public float modifyBaseDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
+        return damage -2;
+    }
+
+    @Override
+    public void updateDynvar(AbstractCard card) {
+        val = getBaseVal(card);
+        modified = false;
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.cost >= 0 && card.baseDamage > 3 && card.type == AbstractCard.CardType.ATTACK && cardCheck(card, c -> usesEnemyTargeting(c));
+    }
+
+    @Override
+    public String getPrefix() {
+        return TEXT[0];
+    }
+
+    @Override
+    public String getSufix() {
+        return TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return rawDescription + String.format(TEXT[2], DESCRIPTION_KEY);
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        addToBot(new ApplyPowerAction(target, AbstractDungeon.player, new PoisonPower(target, AbstractDungeon.player, getBaseVal(card))));
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new PoisonedMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+
+    @Override
+    public String key() {
+        return ID;
+    }
+
+    @Override
+    public int val(AbstractCard card) {
+        return val;
+    }
+
+    @Override
+    public int baseVal(AbstractCard card) {
+        return getBaseVal(card);
+    }
+
+    @Override
+    public boolean modified(AbstractCard card) {
+        return modified;
+    }
+
+    @Override
+    public boolean upgraded(AbstractCard card) {
+        val = getBaseVal(card);
+        modified = card.timesUpgraded != 0 || card.upgraded;
+        upgraded = card.timesUpgraded != 0 || card.upgraded;
+        return upgraded;
+    }
+}
---
+++ src/main/java/CardAugments/cardmods/uncommon/LockingMod.java
@@ -0,0 +1,82 @@
+package CardAugments.cardmods.uncommon;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.blue.LockOn;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+
+public class LockingMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID(LockingMod.class.getSimpleName());
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    public static final int EFFECT = 2;
+    private boolean modifiedBase;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        if (card instanceof LockOn) {
+            card.baseMagicNumber += EFFECT;
+            card.magicNumber += EFFECT;
+            modifiedBase = true;
+        }
+    }
+
+    @Override
+    public float modifyBaseDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
+        return damage * MINOR_DEBUFF;
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        if (!modifiedBase && target != null) {
+            addToBot(new ApplyPowerAction(target, AbstractDungeon.player, new VulnerablePower(target, EFFECT, false)));
+        }
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return allowOrbMods() && card.cost >= 0 && card.type == AbstractCard.CardType.ATTACK && card.baseDamage > 1 && cardCheck(card, c -> usesEnemyTargeting(c));
+    }
+
+    @Override
+    public String getPrefix() {
+        return TEXT[0];
+    }
+
+    @Override
+    public String getSufix() {
+        return TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        if (modifiedBase) {
+            return rawDescription;
+        }
+        return rawDescription + String.format(TEXT[2], EFFECT);
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new LockingMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
