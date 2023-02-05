ca093f47b5b0ee1e89779165777d21620563df6a
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-02-05 18:06:27 -0400

    Add Shielded Mod

---
+++ src/main/java/CardAugments/cardmods/common/ShieldedMod.java
@@ -0,0 +1,125 @@
+package CardAugments.cardmods.common;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import CardAugments.cardmods.DynvarCarrier;
+import CardAugments.util.CalcHelper;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.NextTurnBlockPower;
+
+public class ShieldedMod extends AbstractAugment implements DynvarCarrier {
+    public static final String ID = CardAugmentsMod.makeID(ShieldedMod.class.getSimpleName());
+    public static final String DESCRIPTION_KEY = "!"+ID+"!";
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    private static final int BLOCK = 5;
+    private static final int UPGRADE_BLOCK = 3;
+
+    public int val;
+    public boolean modified;
+    public boolean upgraded;
+
+    public int getBaseVal(AbstractCard card) {
+        int upgrades = card.timesUpgraded;
+        if (upgrades == 0) {
+            return BLOCK;
+        } else if (upgrades < 0) {
+            upgrades *= -1;
+        }
+        return BLOCK + upgrades * UPGRADE_BLOCK;
+    }
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        val = getBaseVal(card);
+        card.cost = card.cost + 1;
+        card.costForTurn = card.cost;
+    }
+
+    @Override
+    public void updateDynvar(AbstractCard card) {
+        val = getBaseVal(card);
+        modified = false;
+    }
+
+    @Override
+    public void onApplyPowers(AbstractCard card) {
+        val = CalcHelper.applyPowersToBlock(getBaseVal(card));
+        modified = val != getBaseVal(card);
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.cost >= 0 && card.type != AbstractCard.CardType.POWER && cardCheck(card, c -> doesntUpgradeCost());
+    }
+
+    @Override
+    public String getPrefix() {
+        return TEXT[0];
+    }
+
+    @Override
+    public String getSufix() {
+        return TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return rawDescription + String.format(TEXT[2], DESCRIPTION_KEY);
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        this.addToBot(new GainBlockAction(AbstractDungeon.player, val));
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.COMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new ShieldedMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+
+    @Override
+    public String key() {
+        return ID;
+    }
+
+    @Override
+    public int val(AbstractCard card) {
+        return val;
+    }
+
+    @Override
+    public int baseVal(AbstractCard card) {
+        return getBaseVal(card);
+    }
+
+    @Override
+    public boolean modified(AbstractCard card) {
+        return modified;
+    }
+
+    @Override
+    public boolean upgraded(AbstractCard card) {
+        val = getBaseVal(card);
+        modified = card.upgraded;
+        upgraded = card.upgraded;
+        return upgraded;
+    }
+}
