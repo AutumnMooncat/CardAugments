1ed459eb2310f29bff469e6d60c1fce48b6a2a88
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-03-12 19:18:00 -0300

    Add compatibility for Oceans Pencil Case

--- src/main/java/CardAugments/patches/CompatibilityPatches.java
+++ src/main/java/CardAugments/patches/CompatibilityPatches.java
@@ -5,8 +5,6 @@ import basemod.helpers.CardModifierManager;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.screens.CombatRewardScreen;
-import visiblecardrewards.patches.NewRewardtypePatch;
-import visiblecardrewards.rewards.SingleCardReward;
 
 public class CompatibilityPatches {
     @SpirePatch2(clz = ModifyRewardsPatch.class, method = "addModifiersRewards", requiredModId = "spirelocations", optional = true)
@@ -19,11 +17,21 @@ public class CompatibilityPatches {
         }
     }
 
-    @SpirePatch2(clz = SingleCardReward.class, method = "init", requiredModId = "visiblecardrewards", optional = true)
+    @SpirePatch2(clz = visiblecardrewards.rewards.SingleCardReward.class, method = "init", requiredModId = "visiblecardrewards", optional = true)
     public static class UseModifiedNames {
         @SpirePostfixPatch
-        public static void plz(SingleCardReward __instance) {
-            if (__instance.type == NewRewardtypePatch.VCR_SINGLECARDREWARD) {
+        public static void plz(visiblecardrewards.rewards.SingleCardReward __instance) {
+            if (__instance.type == visiblecardrewards.patches.NewRewardtypePatch.VCR_SINGLECARDREWARD) {
+                __instance.text = CardModifierManager.onRenderTitle(__instance.card, __instance.card.name);
+            }
+        }
+    }
+
+    @SpirePatch2(clz = oceanmod.rewards.SingleCardReward.class, method = "init", requiredModId = "oceanmod", optional = true)
+    public static class UseModifiedNames2 {
+        @SpirePostfixPatch
+        public static void plz(oceanmod.rewards.SingleCardReward __instance) {
+            if (__instance.type == oceanmod.patches.visiblecardrewards.NewRewardtypePatch.VCR_SINGLECARDREWARD) {
                 __instance.text = CardModifierManager.onRenderTitle(__instance.card, __instance.card.name);
             }
         }
