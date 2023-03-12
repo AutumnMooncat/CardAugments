f7cfcbb577c56dbbc76de296a907bb8154bfff11
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-03-12 19:19:04 -0300

    Projected cards getter now also includes cards in the render queue

--- src/main/java/CardAugments/util/CrossoverHelper.java
+++ src/main/java/CardAugments/util/CrossoverHelper.java
@@ -8,7 +8,9 @@ import java.util.ArrayList;
 public class CrossoverHelper {
     public static class Sisters {
         public static ArrayList<AbstractCard> projectedCards() {
-            return ProjectedCardManager.cards.group;
+            ArrayList<AbstractCard> cards = new ArrayList<>(ProjectedCardManager.cards.group);
+            cards.addAll(ProjectedCardManager.renderQueue.group);
+            return cards;
         }
     }
 }
