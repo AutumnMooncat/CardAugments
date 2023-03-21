29074cbaad3065963fb40f1e613836510959b942
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-03-21 20:07:30 -0300

    Add Chimera Poll command.

--- src/main/java/CardAugments/commands/Chimera.java
+++ src/main/java/CardAugments/commands/Chimera.java
@@ -1,10 +1,12 @@
 package CardAugments.commands;
 
 import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
 import basemod.BaseMod;
 import basemod.DevConsole;
 import basemod.devcommands.ConsoleCommand;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
@@ -13,6 +15,7 @@ public class Chimera extends ConsoleCommand {
     public Chimera() {
         followup.put("hand", ChimeraHand.class);
         followup.put("deck", ChimeraDeck.class);
+        followup.put("poll", ChimeraPoll.class);
         requiresPlayer = true;
         simpleCheck = true;
     }
@@ -38,6 +41,23 @@ public class Chimera extends ConsoleCommand {
         return CardAugmentsMod.modMap.keySet().stream().filter(s -> CardAugmentsMod.modMap.get(s).validCard(card)).collect(Collectors.toCollection(ArrayList::new));
     }
 
+    public static ArrayList<String> getAllMods() {
+        return new ArrayList<>(CardAugmentsMod.modMap.keySet());
+    }
+
+    public static ArrayList<String> getAllValidCards(AbstractAugment a) {
+        ArrayList<String> cardIDs = new ArrayList<>();
+        for (String s : CardLibrary.cards.keySet()) {
+            if (a.validCard(CardLibrary.cards.get(s))) {
+                String cardid = CardLibrary.cards.get(s).cardID.replace(' ', '_');
+                if (!cardIDs.contains(cardid)) {// 248
+                    cardIDs.add(cardid);
+                }
+            }
+        }
+        return cardIDs;
+    }
+
     public static void cmdChimeraHelp() {
         DevConsole.couldNotParse();
         DevConsole.log("options are:");
@@ -45,5 +65,6 @@ public class Chimera extends ConsoleCommand {
         DevConsole.log("* hand add [card id] [mod id] {count} {upgrade amt}");
         DevConsole.log("* deck mod [card id] [mod id]");
         DevConsole.log("* deck add [card id] [mod id] {count} {upgrade amt}");
+        DevConsole.log("* poll [mod id]");
     }
 }
---
+++ src/main/java/CardAugments/commands/ChimeraPoll.java
@@ -0,0 +1,64 @@
+package CardAugments.commands;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.DevConsole;
+import basemod.devcommands.ConsoleCommand;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ChimeraPoll extends ConsoleCommand {
+    public ChimeraPoll() {
+        requiresPlayer = true;
+        minExtraTokens = 1;
+        maxExtraTokens = 1;
+    }
+
+    @Override
+    public void execute(String[] tokens, int depth) {
+        if (CardAugmentsMod.modMap.containsKey(tokens[depth])) {
+            AbstractAugment a = CardAugmentsMod.modMap.get(tokens[depth]);
+            ArrayList<String> validCards = Chimera.getAllValidCards(a);
+            Collections.sort(validCards);
+            String ret = validCards.size()+" valid cards are: ";
+            for (String s : validCards) {
+                ret += " "+s;
+                if (ret.length() > 100) {
+                    DevConsole.log(ret);
+                    ret = "";
+                }
+            }
+            if (!ret.isEmpty()) {
+                DevConsole.log(ret);
+            }
+        } else {
+            DevConsole.log("could not find mod " + tokens[depth]);
+        }
+    }
+
+    @Override
+    public ArrayList<String> extraOptions(String[] tokens, int depth) {
+        ArrayList<String> options = Chimera.getAllMods();
+        if(options.contains(tokens[depth])) {
+            options.clear();
+        } else if(tokens.length > depth + 1) {
+            tooManyTokensError();
+        }
+        return options;
+    }
+
+    @Override
+    public void errorMsg() {
+        cmdChimeraHelp();
+    }
+
+    public static void cmdChimeraHelp() {
+        DevConsole.couldNotParse();
+        DevConsole.log("options are:");
+        DevConsole.log("* * [mod id]");
+        DevConsole.log("* * [mod id]");
+    }
+}
