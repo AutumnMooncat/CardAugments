397cdf5b9fc492cafa4b0d3fe340bc7fd4d6ee90
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-03-21 22:34:51 -0300

    Add Grit Mod

---
+++ src/main/java/CardAugments/cardmods/common/GritMod.java
@@ -0,0 +1,67 @@
+package CardAugments.cardmods.common;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.common.ExhaustAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+public class GritMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID(GritMod.class.getSimpleName());
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.cost != -2 && card.baseBlock > 0;
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        if (card.upgraded) {
+            this.addToBot(new ExhaustAction(1, false));
+        } else {
+            this.addToBot(new ExhaustAction(1, true, false, false));
+        }
+    }
+
+    @Override
+    public String getPrefix() {
+        return TEXT[0];
+    }
+
+    @Override
+    public String getSuffix() {
+        return TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        if (card.upgraded) {
+            return rawDescription + TEXT[3];
+        }
+        return rawDescription + TEXT[2];
+    }
+
+    @Override
+    public void onUpgradeCheck(AbstractCard card) {
+        card.initializeDescription();
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.COMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new GritMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
