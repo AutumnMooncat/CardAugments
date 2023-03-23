8384b539b4a6577f22838be944fe0faae9e12a18
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-03-22 22:26:29 -0300

    Add Armed Mod

---
+++ src/main/java/CardAugments/cardmods/rare/ArmedMod.java
@@ -0,0 +1,110 @@
+package CardAugments.cardmods.rare;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import CardAugments.cardmods.DynvarCarrier;
+import Starlight.util.Wiz;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.common.UpgradeRandomCardAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+public class ArmedMod extends AbstractAugment implements DynvarCarrier {
+    public static final String ID = CardAugmentsMod.makeID(ArmedMod.class.getSimpleName());
+    public static final String DESCRIPTION_KEY = "!"+ID+"!";
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    private static final int EFFECT = 1;
+    private static final int UPGRADE_EFFECT = 1;
+
+    public boolean modified;
+    public boolean upgraded;
+
+    public int getBaseVal(AbstractCard card) {
+        return EFFECT + getEffectiveUpgrades(card) * UPGRADE_EFFECT;
+    }
+
+    @Override
+    public float modifyBaseBlock(float block, AbstractCard card) {
+        return block * MINOR_DEBUFF;
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.cost != -2 && card.baseBlock > 1;
+    }
+
+    @Override
+    public String getPrefix() {
+        return TEXT[0];
+    }
+
+    @Override
+    public String getSuffix() {
+        return TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        if (card.upgraded) {
+            return rawDescription + String.format(TEXT[3], DESCRIPTION_KEY);
+        }
+        return rawDescription + String.format(TEXT[2], DESCRIPTION_KEY);
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        for (int i = 0 ; i < getBaseVal(card); i++) {
+            Wiz.atb(new UpgradeRandomCardAction());
+        }
+    }
+
+    @Override
+    public void onUpgradeCheck(AbstractCard card) {
+        card.initializeDescription();
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new ArmedMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+
+    @Override
+    public String key() {
+        return ID;
+    }
+
+    @Override
+    public int val(AbstractCard card) {
+        return getBaseVal(card);
+    }
+
+    @Override
+    public int baseVal(AbstractCard card) {
+        return getBaseVal(card);
+    }
+
+    @Override
+    public boolean modified(AbstractCard card) {
+        return modified;
+    }
+
+    @Override
+    public boolean upgraded(AbstractCard card) {
+        modified = card.timesUpgraded != 0 || card.upgraded;
+        upgraded = card.timesUpgraded != 0 || card.upgraded;
+        return upgraded;
+    }
+}
