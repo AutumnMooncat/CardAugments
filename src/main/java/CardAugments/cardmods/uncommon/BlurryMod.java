2f626c672541d6389855416dd076de018f1d0cb6
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-03-27 14:27:21 -0300

    Add Blurry Mod

---
+++ src/main/java/CardAugments/cardmods/uncommon/BlurryMod.java
@@ -0,0 +1,64 @@
+package CardAugments.cardmods.uncommon;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import CardAugments.cardmods.DynvarCarrier;
+import Starlight.util.Wiz;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.common.UpgradeRandomCardAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.powers.BlurPower;
+
+public class BlurryMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID(BlurryMod.class.getSimpleName());
+    public static final String DESCRIPTION_KEY = "!"+ID+"!";
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    @Override
+    public float modifyBaseBlock(float block, AbstractCard card) {
+        return block * MODERATE_DEBUFF;
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.cost != -2 && card.baseBlock > 1;
+    }
+
+    @Override
+    public String getPrefix() {
+        return TEXT[0];
+    }
+
+    @Override
+    public String getSuffix() {
+        return TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return rawDescription + String.format(TEXT[2], DESCRIPTION_KEY);
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        Wiz.applyToSelf(new BlurPower(Wiz.adp(), 1));
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.UNCOMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new BlurryMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
