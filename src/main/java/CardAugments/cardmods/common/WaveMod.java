868121b2bae5a0d43d3ff710484eca6ac37aebda
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-03-28 01:18:23 -0300

    Add Wave Mod

---
+++ src/main/java/CardAugments/cardmods/common/WaveMod.java
@@ -0,0 +1,131 @@
+package CardAugments.cardmods.common;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import CardAugments.cardmods.DynvarCarrier;
+import CardAugments.damagemods.PerniciousDamage;
+import CardAugments.util.CalcHelper;
+import basemod.abstracts.AbstractCardModifier;
+import com.evacipated.cardcrawl.mod.stslib.damagemods.DamageModifierManager;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import javax.management.DynamicMBean;
+
+public class WaveMod extends AbstractAugment implements DynvarCarrier {
+    public static final String ID = CardAugmentsMod.makeID(WaveMod.class.getSimpleName());
+    public static final String DESCRIPTION_KEY = "!"+ID+"!";
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    public int baseVal;
+    public int val;
+    public boolean modified;
+    public boolean upgraded;
+
+    public int getBaseVal(AbstractCard card) {
+        return (int) (card.baseDamage - (Math.floor(card.baseDamage * HUGE_DEBUFF)));
+    }
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        baseVal = getBaseVal(card);
+        val = baseVal;
+    }
+
+    @Override
+    public void updateDynvar(AbstractCard card) {
+        val = baseVal;
+        modified = false;
+    }
+
+    @Override
+    public void onApplyPowers(AbstractCard card) {
+        val = CalcHelper.applyPowersToBlock(getBaseVal(card));
+        modified = val != baseVal;
+    }
+
+    @Override
+    public void onUpgradeCheck(AbstractCard card) {
+        int oldBase = baseVal;
+        baseVal = getBaseVal(card);
+        val = baseVal;
+        upgraded = oldBase != baseVal;
+    }
+
+    @Override
+    public float modifyBaseDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
+        return damage * HUGE_DEBUFF;
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.type == AbstractCard.CardType.ATTACK && card.baseDamage > 1 && cardCheck(card, c -> notEthereal(c) && notInnate(c) && notRetain(c));
+    }
+
+    @Override
+    public String getPrefix() {
+        return TEXT[0];
+    }
+
+    @Override
+    public String getSuffix() {
+        return TEXT[1];
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return String.format(TEXT[2], DESCRIPTION_KEY) + rawDescription;
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        this.addToTop(new GainBlockAction(AbstractDungeon.player, val));
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.COMMON;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new WaveMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+
+    @Override
+    public String key() {
+        return ID;
+    }
+
+    @Override
+    public int val(AbstractCard card) {
+        return val;
+    }
+
+    @Override
+    public int baseVal(AbstractCard card) {
+        return getBaseVal(card);
+    }
+
+    @Override
+    public boolean modified(AbstractCard card) {
+        return modified;
+    }
+
+    @Override
+    public boolean upgraded(AbstractCard card) {
+        modified = upgraded;
+        return upgraded;
+    }
+}
