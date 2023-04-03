48db112ff23ca2d4c2204419823529bc5e20eb25
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-03 13:48:02 -0300

    Explosive Mod reworked. Now plays 5 copies of the card at the start of your second turn from now.

--- src/main/java/CardAugments/cardmods/rare/ExplosiveMod.java
+++ src/main/java/CardAugments/cardmods/rare/ExplosiveMod.java
@@ -2,34 +2,49 @@ package CardAugments.cardmods.rare;
 
 import CardAugments.CardAugmentsMod;
 import CardAugments.cardmods.AbstractAugment;
+import CardAugments.cardmods.util.PreviewedMod;
+import CardAugments.patches.InterruptUseCardFieldPatches;
+import CardAugments.powers.BombPower;
+import CardAugments.util.FormatHelper;
+import CardAugments.util.PortraitHelper;
+import CardAugments.util.Wiz;
 import basemod.abstracts.AbstractCardModifier;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import basemod.helpers.CardModifierManager;
+import basemod.patches.com.megacrit.cardcrawl.cards.AbstractCard.MultiCardPreview;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.colorless.TheBomb;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.powers.TheBombPower;
 
 public class ExplosiveMod extends AbstractAugment {
     public static final String ID = CardAugmentsMod.makeID(ExplosiveMod.class.getSimpleName());
     public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
-
-    private static final int EFFECT = 20;
+    public static final int COPIES = 5;
+    private boolean inherentHack = true;
 
     @Override
     public void onInitialApplication(AbstractCard card) {
-        if (card instanceof TheBomb) {
-            card.baseMagicNumber += EFFECT;
-        }
-        card.cost = card.cost + 1;
+        inherentHack = true;
+        AbstractCard preview = card.makeStatEquivalentCopy();
+        inherentHack = false;
+        CardModifierManager.addModifier(preview, new PreviewedMod());
+        MultiCardPreview.add(card, preview);
+        card.cost = 2;
         card.costForTurn = card.cost;
+        card.isEthereal = false;
+        card.exhaust = false;
+        card.target = AbstractCard.CardTarget.SELF;
+        InterruptUseCardFieldPatches.InterceptUseField.interceptUse.set(card, true);
+        if (card.type != AbstractCard.CardType.SKILL) {
+            card.type = AbstractCard.CardType.SKILL;
+            PortraitHelper.setMaskedPortrait(card);
+        }
     }
 
     @Override
     public boolean validCard(AbstractCard card) {
-        return card.cost >= 0 && card.rarity != AbstractCard.CardRarity.BASIC && card.rarity != AbstractCard.CardRarity.COMMON && cardCheck(card, c -> doesntUpgradeCost());
+        return noShenanigans(card) && (card.cost == 0 || card.cost == 1) && card.type == AbstractCard.CardType.ATTACK && cardCheck(card, c -> doesntUpgradeCost() /*&& customCheck(check -> check.target == AbstractCard.CardTarget.ALL || check.target == AbstractCard.CardTarget.ALL_ENEMY)*/);
     }
 
     @Override
@@ -44,20 +59,43 @@ public class ExplosiveMod extends AbstractAugment {
 
     @Override
     public String modifyDescription(String rawDescription, AbstractCard card) {
-        if (card instanceof TheBomb) {
-            return rawDescription;
+        return (card.isInnate ? TEXT[3] : "") + String.format(TEXT[2], COPIES, FormatHelper.prefixWords(card.name, "*"));
+    }
+
+    @Override
+    public void onUpgradeCheck(AbstractCard card) {
+        for (AbstractCard c : MultiCardPreview.multiCardPreview.get(card)) {
+            if (CardModifierManager.hasModifier(c, PreviewedMod.ID)) {
+                c.upgrade();
+                CardModifierManager.testBaseValues(c);
+                c.initializeDescription();
+            }
         }
-        return rawDescription + String.format(TEXT[2], EFFECT);
+        card.initializeDescription();
     }
 
     @Override
     public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
-        this.addToBot(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new TheBombPower(AbstractDungeon.player, 3, EFFECT), 3));
+        AbstractCard preview = null;
+        for (AbstractCard c : MultiCardPreview.multiCardPreview.get(card)) {
+            if (CardModifierManager.hasModifier(c, PreviewedMod.ID)) {
+                preview = c;
+            }
+        }
+        if (preview != null) {
+            AbstractCard copy = preview.makeStatEquivalentCopy();
+            Wiz.applyToSelf(new BombPower(AbstractDungeon.player, 2, COPIES, copy));
+        }
     }
 
     @Override
     public AugmentRarity getModRarity() {
         return AugmentRarity.RARE;
+    }
+
+    @Override
+    public boolean isInherent(AbstractCard card) {
+        return inherentHack;
     }
 
     @Override
---
+++ src/main/java/CardAugments/powers/BombPower.java
@@ -0,0 +1,101 @@
+package CardAugments.powers;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.util.FormatHelper;
+import Starlight.powers.ProvidencePower;
+import basemod.ReflectionHacks;
+import com.badlogic.gdx.Gdx;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.NonStackablePower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.SilentGainPowerEffect;
+
+import java.util.ArrayList;
+
+public class BombPower extends AbstractPower implements NonStackablePower {
+    public static final String POWER_ID = CardAugmentsMod.makeID(BombPower.class.getSimpleName());
+    private static final PowerStrings TEXT = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    private final AbstractCard card;
+    private final int cards;
+    private float flashTimer = -1f;
+    private final ArrayList<AbstractGameEffect> array;
+
+    public BombPower(AbstractCreature owner, int amount, int cards, AbstractCard card) {
+        name = TEXT.NAME + card.name;
+        ID = POWER_ID;
+        this.amount = amount;
+        this.cards = cards;
+        this.owner = owner;
+        this.card = card.makeStatEquivalentCopy();
+        type = PowerType.BUFF;
+        isTurnBased = false;
+        this.loadRegion("the_bomb");
+        updateDescription();
+        this.array = ReflectionHacks.getPrivateInherited(this, ProvidencePower.class, "effect");
+
+    }
+
+    public void update(int slot) {
+        super.update(slot);
+        if (flashTimer != -1f) {
+            this.flashTimer += Gdx.graphics.getDeltaTime();
+            if (this.flashTimer > 1.0F) {
+                this.array.add(new SilentGainPowerEffect(this));
+                this.flashTimer = 0.0F;
+            }
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        if (amount == 1) {
+            description = String.format(TEXT.DESCRIPTIONS[1], cards, FormatHelper.prefixWords(card.name, "#y"));
+        } else {
+            description = String.format(TEXT.DESCRIPTIONS[0], amount, cards, FormatHelper.prefixWords(card.name, "#y"));
+        }
+
+    }
+
+    @Override
+    public void atStartOfTurnPostDraw() {
+        if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+            this.addToBot(new ReducePowerAction(this.owner, this.owner, this, 1));
+            if (this.amount == 1) {
+                this.flash();
+                addToBot(new AbstractGameAction() {
+                    @Override
+                    public void update() {
+                        for (int i = 0 ; i < cards ; i++) {
+                            AbstractCard tmp = card.makeSameInstanceOf();
+                            AbstractDungeon.player.limbo.addToBottom(tmp);
+                            tmp.current_x = card.current_x;
+                            tmp.current_y = card.current_y;
+                            tmp.target_x = (float) Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
+                            tmp.target_y = (float)Settings.HEIGHT / 2.0F;
+
+                            tmp.purgeOnUse = true;
+                            AbstractDungeon.actionManager.addCardQueueItem(new CardQueueItem(tmp, true, card.energyOnUse, true, true), true);// 68
+                        }
+                        this.isDone = true;
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    public void atEndOfTurn(boolean isPlayer) {
+        if (amount == 1) {
+            flashTimer = 1f;
+        }
+    }
+}
