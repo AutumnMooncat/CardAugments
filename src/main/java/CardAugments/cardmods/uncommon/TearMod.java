0b4fb530ffa2709c5e727cc2ebc2c53862183978
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-04 20:24:52 -0300

    Tear Mod now deals !D! damage to a random enemy instead of flat 7(9).

--- src/main/java/CardAugments/cardmods/uncommon/TearMod.java
+++ src/main/java/CardAugments/cardmods/uncommon/TearMod.java
@@ -1,11 +1,9 @@
 package CardAugments.cardmods.uncommon;
 
 import CardAugments.CardAugmentsMod;
-import CardAugments.actions.AndTearAction;
 import CardAugments.cardmods.AbstractAugment;
-import CardAugments.cardmods.DynvarCarrier;
-import CardAugments.util.CalcHelper;
 import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.defect.NewRipAndTearAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -13,26 +11,9 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class TearMod extends AbstractAugment implements DynvarCarrier {
+public class TearMod extends AbstractAugment {
     public static final String ID = CardAugmentsMod.makeID(TearMod.class.getSimpleName());
-    public static final String DESCRIPTION_KEY = "!"+ID+"!";
     public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
-
-    private static final int DAMAGE = 7;
-    private static final int UPGRADE_DAMAGE = 2;
-
-    public int val;
-    public boolean modified;
-    public boolean upgraded;
-
-    public int getBaseVal(AbstractCard card) {
-        return DAMAGE + getEffectiveUpgrades(card) * UPGRADE_DAMAGE;
-    }
-
-    @Override
-    public void onInitialApplication(AbstractCard card) {
-        val = getBaseVal(card);
-    }
 
     @Override
     public float modifyBaseDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
@@ -40,20 +21,8 @@ public class TearMod extends AbstractAugment implements DynvarCarrier {
     }
 
     @Override
-    public void updateDynvar(AbstractCard card) {
-        val = getBaseVal(card);
-        modified = false;
-    }
-
-    @Override
-    public void onApplyPowers(AbstractCard card) {
-        val = CalcHelper.applyPowers(getBaseVal(card));
-        modified = val != getBaseVal(card);
-    }
-
-    @Override
     public boolean validCard(AbstractCard card) {
-        return card.cost != -2 && card.baseDamage > 1 && card.type == AbstractCard.CardType.ATTACK;
+        return card.cost != -2 && card.baseDamage >= 3 && card.type == AbstractCard.CardType.ATTACK;
     }
 
     @Override
@@ -68,12 +37,12 @@ public class TearMod extends AbstractAugment implements DynvarCarrier {
 
     @Override
     public String modifyDescription(String rawDescription, AbstractCard card) {
-        return rawDescription + String.format(TEXT[2], DESCRIPTION_KEY);
+        return rawDescription + TEXT[2];
     }
 
     @Override
     public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
-        addToBot(new AndTearAction(getBaseVal(card)));
+        addToBot(new NewRipAndTearAction(card));
     }
 
     @Override
@@ -89,33 +58,5 @@ public class TearMod extends AbstractAugment implements DynvarCarrier {
     @Override
     public String identifier(AbstractCard card) {
         return ID;
-    }
-
-    @Override
-    public String key() {
-        return ID;
-    }
-
-    @Override
-    public int val(AbstractCard card) {
-        return val;
-    }
-
-    @Override
-    public int baseVal(AbstractCard card) {
-        return getBaseVal(card);
-    }
-
-    @Override
-    public boolean modified(AbstractCard card) {
-        return modified;
-    }
-
-    @Override
-    public boolean upgraded(AbstractCard card) {
-        val = getBaseVal(card);
-        modified = card.timesUpgraded != 0 || card.upgraded;
-        upgraded = card.timesUpgraded != 0 || card.upgraded;
-        return upgraded;
     }
 }
