7a93da429e8e91a7fb51d51ce8a85a4e252e976e
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-05 01:33:16 -0300

    Add BlasphemousMod

---
+++ src/main/java/CardAugments/cardmods/rare/BlasphemousMod.java
@@ -0,0 +1,92 @@
+package CardAugments.cardmods.rare;
+
+import CardAugments.CardAugmentsMod;
+import CardAugments.cardmods.AbstractAugment;
+import CardAugments.util.Wiz;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.watcher.EndTurnDeathPower;
+
+public class BlasphemousMod extends AbstractAugment {
+    public static final String ID = CardAugmentsMod.makeID(BlasphemousMod.class.getSimpleName());
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+
+    boolean modMagic;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        if (cardCheck(card, c -> doesntDowngradeMagic() && c.baseMagicNumber >= 1)) {
+            modMagic = true;
+        }
+    }
+
+    @Override
+    public float modifyBaseDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
+        if (card.baseDamage > 0) {
+            return damage * 5;
+        }
+        return damage;
+    }
+
+    @Override
+    public float modifyBaseBlock(float block, AbstractCard card) {
+        if (card.baseBlock > 0) {
+            return block * 5;
+        }
+        return block;
+    }
+
+    @Override
+    public float modifyBaseMagic(float magic, AbstractCard card) {
+        if (modMagic) {
+            return magic * 5;
+        }
+        return magic;
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return card.baseDamage > 0 || card.baseBlock > 0 || cardCheck(card, c -> doesntDowngradeMagic() && c.baseMagicNumber >= 1);
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return rawDescription + TEXT[2];
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        this.addToBot(new ApplyPowerAction(Wiz.adp(), Wiz.adp(), new EndTurnDeathPower(Wiz.adp())));
+    }
+
+    @Override
+    public String getPrefix() {
+        return TEXT[0];
+    }
+
+    @Override
+    public String getSuffix() {
+        return TEXT[1];
+    }
+
+    @Override
+    public AugmentRarity getModRarity() {
+        return AugmentRarity.RARE;
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new BlasphemousMod();
+    }
+
+    @Override
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
