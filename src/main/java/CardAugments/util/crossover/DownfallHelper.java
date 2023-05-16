e52117628182791a55e006a13ac87e0cb9dc2e99
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-05-16 13:14:42 -0300

    Add unused helper for downfall

---
+++ src/main/java/CardAugments/util/crossover/DownfallHelper.java
@@ -0,0 +1,9 @@
+package CardAugments.util.crossover;
+
+import downfall.patches.EvilModeCharacterSelect;
+
+public class DownfallHelper {
+    public static boolean isDownfallMode() {
+        return EvilModeCharacterSelect.evilMode;
+    }
+}
